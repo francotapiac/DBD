aa9f2b45449a63ce57535b3726ff3452b0021142
Unknown <franco.tapia.c@usach.cl>
2018-11-28 18:18:59 -0300

    Entidad Paquete

---
+++ database/migrations/2014_11_28_211352_create_paquetes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaquetesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paquetes', function (Blueprint $table) {
+            $table->increments('id_paquete');
+            $table->decimal('precio_por_persona');
+            $table->text('descripcion');
+            $table->decimal('descuento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paquetes');
+    }
+}
