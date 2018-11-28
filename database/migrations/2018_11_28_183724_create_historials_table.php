9dbfc06cf60d63a5885b71ed0516a9d0ff1cacab
Maximiliano Arévalo Sáez <maximiliano.arevalo@usach.cl>
2018-11-28 15:40:58 -0300

    Migracion Historial

---
+++ database/migrations/2018_11_28_183724_create_historials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistorialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historials', function (Blueprint $table) {
+            $table->increments('id_historial');
+            $table->text('descripcion');
+            $table->string('accion',64);
+            $table->date('fecha_accion');
+            $table->time('hora_accion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historials');
+    }
+}
